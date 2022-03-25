d20b8ea39793358b402a9089eff0482d1e79ba0c
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2022-03-24 20:01:11 -0500

    adding healthcheck

---
+++ app/controllers/health.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import config from "../config.js";
+
+class HealthController {
+  async getAll(req, res, next) {
+    try {
+      const healthStatus = {
+        env: config.NODE_ENV,
+        title: config.APP_TITLE,
+        description: config.APP_DESCRIPTION,
+        version: config.APP_VERSION,
+      };
+      res.status(200).json(healthStatus);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default new HealthController();
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import healthRouter from "./health.js";
 import heroRouter from "./hero.js";
 
 const router = Router();
 
+router.use("/status", healthRouter);
 router.use("/heroes", heroRouter);
 
 export default router;
